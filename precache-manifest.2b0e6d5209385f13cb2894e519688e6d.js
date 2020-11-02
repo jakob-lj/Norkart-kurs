13b20291552cbd245ef44973590f91fbe627affe
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-11-02 17:41:36 +0100

    Updates

---
+++ precache-manifest.2b0e6d5209385f13cb2894e519688e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99222bce84e3487d72960212fe396161",
+    "url": "/Norkart-kurs/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/Norkart-kurs/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "1f95aeda1e5f765bed5e",
+    "url": "/Norkart-kurs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/Norkart-kurs/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Norkart-kurs/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f95aeda1e5f765bed5e",
+    "url": "/Norkart-kurs/static/js/main.5e49fefd.chunk.js"
+  },
+  {
+    "revision": "c786862a5ea48a8a77d5",
+    "url": "/Norkart-kurs/static/js/runtime-main.1144851e.js"
+  }
+]);
